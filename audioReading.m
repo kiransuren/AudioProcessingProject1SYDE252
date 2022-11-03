30a4ebeaba630afc0ddb39ef26aaa2c32c131e8e
kiransuren <kirank.suren@gmail.com>
2022-11-03 18:39:43 -0400

    completed part 1-preparations

--- audioReading.m
+++ audioReading.m
@@ -7,20 +7,36 @@
 
 size(birdsAudioData,2)       % 1 channels -> stereo
 sound(birdsAudioData, birdsSampleRate);
-audiowrite('Output/newBirdsAudioData.wav', birdsAudioData, birdsSampleRate);
+audiowrite('newBirdsAudioData.wav', birdsAudioData, birdsSampleRate);
 
 size(speechAudioData,2)     % 1 channel -> mono
 sound(speechAudioData, speechSampleRate);
-audiowrite('Output/newSpeechAudioData.wav', speechAudioData, speechSampleRate);
+audiowrite('newSpeechAudioData.wav', speechAudioData, speechSampleRate);
 
 size(drumAudioData,2)       % 2 channels -> stereo
-%TODO: add columns to make it a single channel
 monoDrumAudioData = drumAudioData(:,1) + drumAudioData(:,2);
 sound(monoDrumAudioData, drumSampleRate);
-audiowrite('Output/newMonoDrumAudioData.wav', monoDrumAudioData, drumSampleRate);
+audiowrite('newMonoDrumAudioData.wav', monoDrumAudioData, drumSampleRate);
 
+% resampling
+fprintf('old sample %d\n', birdsSampleRate);
+s = 16000/birdsSampleRate;
+y = resample(birdsAudioData, 1600, birdsSampleRate);
+fprintf('new sample %d\n', s*birdsSampleRate);
+sound(y, s*birdsSampleRate);
+
+%   Plotting audio waveforms
+subplot(3,1,1)
 xlabel('t')
 ylabel('audioform')
 plot(birdsAudioData)
+
+subplot(3,1,2)
+xlabel('t')
+ylabel('audioform')
 plot(speechAudioData)
+
+subplot(3,1,3)
+xlabel('t')
+ylabel('audioform')
 plot(monoDrumAudioData)
