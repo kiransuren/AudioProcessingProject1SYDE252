9353904bac6e5ec6f92cc21f439e4742cfdbc048
kiransuren <kirank.suren@gmail.com>
2022-11-25 18:57:38 -0500

    add comments to mean and syllablesSpeech

--- meanFilter.m
+++ meanFilter.m
@@ -1,5 +1,5 @@
 function y = meanFilter(x,L)
-    b = (1/L) * ones(1, L);
-    a = 1;
-    y = filter(b,a,x);
+    b = (1/L) * ones(1, L); % construct transfer function numerator
+    a = 1;                  % construct transfer function denominator
+    y = filter(b,a,x);      % apply transfer function on input signal
 end
--- syllablesSpeech.m
+++ syllablesSpeech.m
@@ -1,42 +1,49 @@
 function numPeaks = syllablesSpeech()
-    speechAudioData = Init('Speech.wav');
+    speechAudioData = Init('Speech.wav'); % get raw speech data
     
-    y = meanFilter(speechAudioData, 20);
+    y = meanFilter(speechAudioData, 20);  % reduce noise with mean filter
     
-    rawPeakList = peakDetector(y, 50)';
-    peakList = meanFilter(rawPeakList,20);
-    peakList = meanFilter(peakList,21);
-    finalList = findpeaks(peakList);
+    rawPeakList = peakDetector(y, 50)';     % sliding window peak detection
+    peakList = meanFilter(rawPeakList,20);  % cascading mean filter level1
+    peakList = meanFilter(peakList,21);     % cascading mean filter level2
+    finalList = findpeaks(peakList);        % final find peaks calculation
     
+    % Plot raw audio data
     subplot(5,1,1)
     plot(speechAudioData)
     xlabel('t')
     ylabel('Audioform')
     title('Raw Speech Audio Data')
     
+    % Plot mean filtered audio data
     subplot(5,1,2)
     plot(y)
     xlabel('t')
     ylabel('Audioform')
     title('Mean Filtered Speech Audio Data')
     
+    % Plot raw peak regions
     subplot(5,1,3)
     plot(rawPeakList)
     xlabel('t')
     ylabel('Audioform')
     title('Raw Peak Regions')
     
+    % Plot filtered peak regions
     subplot(5,1,4)
     plot(peakList)
     xlabel('Peak Index')
     ylabel('Value')
     title('Filtered Peak Regions')
     
+    % Plot final peak values 
     subplot(5,1,5)
     plot(finalList)
     xlabel('Peak Index')
     ylabel('Value')
     title('Peak Values')
+
+    % Get number of peaks fron final list
     numPeaks = size(finalList,1);
     fprintf("\nThe number of syllables detected in this audio clip is %d\n", size(finalList,1));
 end
