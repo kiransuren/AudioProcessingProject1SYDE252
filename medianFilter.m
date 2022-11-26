257bd92ad2e704287d4e17b78f6127752c4b92c2
Jolie Girgis <j4girgis@uwaterloo.ca>
2022-11-25 21:34:28 -0500

    comment medianFilter & silentDetection

--- medianFilter.m
+++ medianFilter.m
@@ -1,7 +1,10 @@
 function F = medianFilter(x, L)
     y = [];
     for i=1:length(x)
-        section = [];
+        section = []; % Stores the section the median should be taken on
+        
+        % For an index value that doesn't exist, consider the value
+        % as 0, otherwise take the value at the valid index
         if i < L        
             for k=1:(L-i)
                 section(end+1,1) = 0;
@@ -9,12 +12,14 @@ function F = medianFilter(x, L)
             for m=1:i
                 section(end+1,1) = x(m);
             end
-        end
+        end 
         if i >= L
             for j=(i-L+1):i
                 section(end+1,1) = x(j);
             end
         end
+        
+        % Get the median for the section
         y(i,1) = median(section);
     end
     F = y;
--- silenceDetection.m
+++ silenceDetection.m
@@ -1,6 +1,9 @@
 function F = silenceDetection(x, L, threshold)
+    % Apply the median filter
     y = medianFilter(x,L);
     for i=1:length(y)
+        % For a value that is between the threshold, consider it as part of
+        % a silent region (=0), otherwise give it a value of 1
         if y(i) < threshold && y(i) > -threshold 
             result(i,1) = 0;
         else
